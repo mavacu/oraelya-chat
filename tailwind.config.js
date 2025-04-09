5cba74d203ba4bac2734e920ffed84b955a81632
MavacuAI <marius@mavacu.com>
2025-04-09 21:01:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oraelya: ['"Playfair Display"', 'serif'],
+        body: ['"Inter"', 'sans-serif'],
+      },
+      colors: {
+        mysticBlack: '#1a1a2e',
+        mysticPurple: '#53354a',
+        goldLight: '#d4af37',
+      },
+      backgroundImage: {
+        'mystic-gradient': 'linear-gradient(to bottom, #1a1a2e, #53354a)',
+      },
+    },
   },
   plugins: [],
-}
+};
