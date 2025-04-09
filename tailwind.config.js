1f11655aacbcdc5d3289a55b26cfd123c585ba2a
MavacuAI <marius@mavacu.com>
2025-04-09 23:52:02 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './styles/**/*.{css}'
+    './styles/**/*.{css}',
   ],
   theme: {
     extend: {
@@ -18,15 +18,6 @@ module.exports = {
       },
       backgroundImage: {
         'mystic-gradient': 'linear-gradient(to bottom, #1a1a2e, #53354a)',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-      },
-      animation: {
-        'fade-in': 'fadeIn 1.2s ease-in-out',
       },
     },
   },
